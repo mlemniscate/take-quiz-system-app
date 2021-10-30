9dbf45ed26aa5ded46893fcca50bf4c9de694d4e
Milad Abdi <miladabdi1374@gmail.com>
2021-10-30 17:04:32 +0330

    declare empty project.

---
+++ src/main/java/ir/maktabsharif/Mk56FinalApplication.java
@@ -0,0 +1,13 @@
+package ir.maktabsharif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mk56FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mk56FinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/maktabsharif/Mk56FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktabsharif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mk56FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
